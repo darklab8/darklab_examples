733b65df53370a333f64cd476354a9afc950838b
dd84ai <dd84ai@gmail.com>
2022-10-20 23:47:29 +0200

    feat: decorated normal descriptor

---
+++ decorators/decorated_descriptor_in_class_instance.py
@@ -0,0 +1,60 @@
+class PropertyName:
+
+    def __set_name__(self, owner, name):
+        self.public_name = name
+        self.private_name = '_' + name
+
+    def __init__(self, default):
+        self.default = default
+
+    def __get__(self, obj, objtype=None):
+        try:
+            value = getattr(obj, self.private_name)
+        except AttributeError:
+            value = self.default
+        return value
+
+    def __set__(self, obj, value):
+        setattr(obj, self.private_name, value)
+
+
+class DecriptorDecorator:
+    def __set_name__(self, owner, name):
+        setattr(self.descriptor,"public_name", name)
+        setattr(self.descriptor,"private_name", f"_{name}")
+
+    def __init__(self, descriptor):
+        self.descriptor = descriptor
+
+    def __get__(self, obj, objtype):
+        returned = self.descriptor.__get__(obj, objtype)
+        print(f"get logic, {obj=}, {objtype=}, {returned=}")
+        return returned
+
+    def __set__(self, obj, value: str):
+        returned = self.descriptor.__set__(obj, value)
+        print(f"set logic, {obj=}, {value=}, {returned=}")
+        return returned
+
+
+class Person:
+    name = DecriptorDecorator(PropertyName(default="abc"))
+    family = PropertyName(default="yay")
+
+    def __init__(self, _attr = "dev"):
+        self.attr = _attr
+
+if __name__=="__main__":
+    Bob = Person()
+    Martin = Person()
+
+    print(f"{Bob.name=},{Bob.family=}")
+    print(f"{Martin.name=},{Martin.family=}")
+
+    Bob.name = "123"
+    Bob.family = "asd"
+    Martin.name = "1234"
+    
+
+    print(f"{Bob.name=},{Bob.family=}")
+    print(f"{Martin.name=},{Martin.family=}")
